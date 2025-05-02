ac3abe5b5b1db2659f910d6d104fb9b872b8be0d
prakul19 <prakulagarwal5@gmail.com>
2025-05-02 10:07:00 +0530

    [prakul] stats: fixed debt service bugs

--- src/main/java/com/cg/fairshare/model/Debt.java
+++ src/main/java/com/cg/fairshare/model/Debt.java
@@ -8,22 +8,28 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString(exclude = {"fromUser", "toUser", "group"})
+@EqualsAndHashCode(exclude = {"fromUser", "toUser", "group"})
 public class Debt {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "from_user_id")
     private User fromUser;
+
     @ManyToOne
     @JoinColumn(name = "to_user_id")
     private User toUser;
+
     @ManyToOne
     @JoinColumn(name = "group_id")
     private Group group;
+
     private Double amount;
+
     @Builder.Default
     private boolean isActive = true;
 }
-
-
--- src/main/java/com/cg/fairshare/model/ExpenseShare.java
+++ src/main/java/com/cg/fairshare/model/ExpenseShare.java
@@ -8,15 +8,21 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString(exclude = {"expense", "user"})
+@EqualsAndHashCode(exclude = {"expense", "user"})
 public class ExpenseShare {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     @JoinColumn(name = "expense_id")
     private Expense expense;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
     private Double amount;
 }
