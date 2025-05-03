8703536a3d084c5fad3266bfb81a67109d4bd16c
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-05-03 13:50:21 +0530

    [Naam Simran Singh Gill] ADD: Added Validation

--- src/main/java/com/cg/fairshare/dto/ChangePasswordRequest.java
+++ src/main/java/com/cg/fairshare/dto/ChangePasswordRequest.java
@@ -1,13 +1,17 @@
 package com.cg.fairshare.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class ChangePasswordRequest {
     private String oldPassword;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, message = "Password should be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z]).{8,}$", message = "Password must contain one letter and one number")
     private String newPassword;
 
-    // getters & setters
-    public String getOldPassword() { return oldPassword; }
-    public void setOldPassword(String oldPassword) { this.oldPassword = oldPassword; }
-
-    public String getNewPassword() { return newPassword; }
-    public void setNewPassword(String newPassword) { this.newPassword = newPassword; }
 }
